395d24f338b5dae496ca53abb9618b14425650be
Laurin Klein <160551518+klein2712@users.noreply.github.com>
2025-05-05 11:11:37 +0200

    Update generate_promt_streamlit.py

--- generate_promt_streamlit.py
+++ generate_promt_streamlit.py
@@ -79,7 +79,7 @@ if 'current_step' not in st.session_state:
     st.session_state.prompt = ""
 
 # Title and description
-st.title("Exam Prompt Generator (Test Version)")
+st.title("Exam Prompt Generator (Version 2)")
 st.markdown("Erstelle einen angepassten Prompt um mithilfe eines LLM (GPT, Gemini, Claude etc.) eine Klausur zu generieren.")
 st.markdown("Je genauer die Angaben, desto besser das Ergebnis")
 
